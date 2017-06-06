7f6a8419482f8d6262f42284d951a750ee5039fe
James Dombro <jsd007@me.com>
2017-06-06 15:26:12 -0400

    Lab 2

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ function handleSubmit(ev) {
     const favoriteColor = f.favoriteColor.value
     const age = f.age.value
 
-    const colorDiv = `<div style="background-color: ${favoriteColor}; width=: 100px; height: 50px;"></div>`
-
+    //const colorDiv = `<div style="background-color: ${favoriteColor}; width=: 100px; height: 50px;"></div>`
+    const colorDiv = favoriteColor
     // const boldedName = document.createElement('strong')
     // boldedName.textContent = name
 
@@ -17,14 +17,27 @@ function handleSubmit(ev) {
     // details.innerHTML += `<strong>${name}</strong>`
     // details.appendChild(boldedName)
 
+    const newUl = document.createElement("ul")
+    details.appendChild(newUl)
+    const lName = document.createElement("li")
+    lName.textContent = title + " " + name
+    details.appendChild(lName)
+    const lColor = document.createElement("li")
+    lColor.textContent = colorDiv
+    details.appendChild(lColor)
+    const lAge = document.createElement("li")
+    lAge.textContent = age
+    details.appendChild(lAge)
 
 
-    details.innerHTML = `<ul>
-    <li>Name: ${name}</li>
-    <li>Favorite Color: ${colorDiv}</li>
-    <li>Age: ${age}</li>
-    </ul>
-    `
+
+
+    // details.innerHTML = `<ul>
+    // <li>Name: ${name}</li>
+    // <li>Favorite Color: ${colorDiv}</li>
+    // <li>Age: ${age}</li>
+    // </ul>
+    // `
 
     document.querySelector('h1').textContent = title + " " + name
     document.querySelector('p1').textContent = "Welcome " + title + " " + name
