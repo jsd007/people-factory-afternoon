a0099d768dc53721bc2fcaae6c3babea56de2c36
James Dombro <jsd007@me.com>
2017-06-06 16:26:54 -0400

    Build List in Seperate Function

--- index.js
+++ index.js
@@ -1,5 +1,30 @@
 const personForm = document.querySelector('form#personForm')
 
+function renderColor(color) {
+   const div = document.createElement('div')
+   div.style.backgroundColor = color
+   div.style.width = '100px'
+   div.style.height = '50px'
+    return div
+}
+
+function renderListItem(label, value) {
+    const item = document.createElement('li')
+    item.textContent = `${label}: ${value}`
+    return item
+}
+
+function renderList(personData){
+    const list = document.createElement('ul')
+    //call renderlist item w/ loop
+    //gives us an array of data
+    Object.keys(personData).map(function(label) {
+        const item = renderListItem(label, personData[label])
+        list.appendChild(item)
+})
+    return list
+}
+
 function handleSubmit(ev) {
     ev.preventDefault()
     const f = ev.target
@@ -8,8 +33,15 @@ function handleSubmit(ev) {
     const favoriteColor = f.favoriteColor.value
     const age = f.age.value
 
-    //const colorDiv = `<div style="background-color: ${favoriteColor}; width=: 100px; height: 50px;"></div>`
-    const colorDiv = favoriteColor
+    const person = {
+        name: f.personName.value,
+        favoriteColor: f.favoriteColor.value,
+        age: f.age.value,
+    }
+
+    details.appendChild(renderList(person))
+
+    // const colorDiv = `<div style="background-color: ${favoriteColor}; width=: 100px; height: 50px;"></div>`
     // const boldedName = document.createElement('strong')
     // boldedName.textContent = name
 
@@ -17,17 +49,17 @@ function handleSubmit(ev) {
     // details.innerHTML += `<strong>${name}</strong>`
     // details.appendChild(boldedName)
 
-    const newUl = document.createElement("ul")
-    details.appendChild(newUl)
-    const lName = document.createElement("li")
-    lName.textContent = title + " " + name
-    details.appendChild(lName)
-    const lColor = document.createElement("li")
-    lColor.textContent = colorDiv
-    details.appendChild(lColor)
-    const lAge = document.createElement("li")
-    lAge.textContent = age
-    details.appendChild(lAge)
+    // const newUl = document.createElement("ul")
+    // details.appendChild(newUl)
+    // const lName = document.createElement("li")
+    // lName.textContent = "Name: " + title + " " + name
+    // details.appendChild(lName)
+    // const lColor = document.createElement("li")
+    // lColor.innerHTML = `Color: ${renderColor(favoriteColor)}`
+    // details.appendChild(lColor)
+    // const lAge = document.createElement("li")
+    // lAge.textContent = "Age: "+ age
+    // details.appendChild(lAge)
 
 
 
