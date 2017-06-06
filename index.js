7d506b5db72f37096a82d91f01a3d3b24f7d963b
James Dombro <jsd007@me.com>
2017-06-06 14:46:17 -0400

    Display Fav Color

--- index.js
+++ index.js
@@ -7,16 +7,21 @@ function handleSubmit(ev) {
     const name = f.personName.value
     const favoriteColor = f.favoriteColor.value
     const age = f.age.value
-    const boldedName = document.createElement('strong')
-    boldedName.textContent = name
+
+    const colorDiv = `<div style="background-color: ${favoriteColor}; width=: 100px; height: 50px;"></div>`
+
+    // const boldedName = document.createElement('strong')
+    // boldedName.textContent = name
 
     // const details = document.querySelector('#details')
     // details.innerHTML += `<strong>${name}</strong>`
     // details.appendChild(boldedName)
 
+
+
     details.innerHTML = `<ul>
     <li>Name: ${name}</li>
-    <li>Favorite Color: ${favoriteColor}</li>
+    <li>Favorite Color: ${colorDiv}</li>
     <li>Age: ${age}</li>
     </ul>
     `
